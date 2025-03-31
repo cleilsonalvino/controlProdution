3cc9173d9682ed6abddb948030870d7083c6684f
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-03-30 22:53:09 -0300

    aaaa

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // A pasta de saída para o build de produção
+  },
 })
