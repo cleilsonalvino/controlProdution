a1141d78b1ce89dd532143b9af607de0dcdae528
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-04-13 01:36:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Não é necessário definir process.env explicitamente em Vite
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
   build: {
     outDir: 'dist', // Mantém a pasta de saída como 'dist'
   },
